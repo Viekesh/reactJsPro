6ce97c0265fea4756fa0d71d759493ae56cd5918
viekesh <vikeshgaikwad1@gmail.com>
2022-02-15 01:06:59 +0530

    add sass and styling app

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import Board from './Components/Board';
 
 export default () => (
-  <>
+  <div className="app">
+    <h1>Tic-Tac-Toe</h1>
     <Board />
-  </>
+  </div>
 );
