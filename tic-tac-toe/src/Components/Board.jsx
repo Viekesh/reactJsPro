6d94500fa4436c46ae82bfa6bca9c8a0453954d8
viekesh <vikeshgaikwad1@gmail.com>
2022-02-15 15:28:28 +0530

    added board state and also react event

--- tic-tac-toe/src/Components/Board.jsx
+++ tic-tac-toe/src/Components/Board.jsx
@@ -1,31 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Square from './Square';
 import './Styles/root.scss';
 
 const Board = () => {
+
+    const [board, setBoard] = useState(Array(9).fill(null));
+
+    const [IsXNext, setIsXNext] = useState(false);
+
+    console.log(board);
+
+    const handleSquareClick = (position) => {
+
+        if (board[position]) {
+            return;
+        }
+
+
+        setBoard((prev) => {
+            return prev.map((square, pos) => {
+                if (pos === position) {
+                    return IsXNext ? 'X' : 'O';
+                }
+
+
+                return square;
+            })
+        })
+
+        setIsXNext(prev => !prev)
+    };
+
+    const renderSquare = (position) => {
+        return (
+            <Square
+                value={board[position]}
+                onclick={() => handleSquareClick(position)}
+            />
+        );
+    };
+
     return (
         <div className='board'>
 
             <div className='board-row'>
-                <Square value={0} />
-                <Square value={1} />
-                <Square value={2} />
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+                {/* <Square value={board[0]} />
+                <Square value={board[1]} />
+                <Square value={board[2]} /> */}
             </div>
 
             <div className='board-row'>
-                <Square value={3} />
-                <Square value={4} />
-                <Square value={5} />
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+                {/* <Square value={board[3]} />
+                <Square value={board[4]} />
+                <Square value={board[5]} /> */}
             </div>
 
             <div className='board-row'>
-                <Square value={6} />
-                <Square value={7} />
-                <Square value={8} />
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+                {/* <Square value={board[6]} />
+                <Square value={board[7]} />
+                <Square value={board[8]} /> */}
             </div>
-            
+
         </div>
-    )
-}
+    );
+};
 
 export default Board;
--- tic-tac-toe/src/Components/Square.jsx
+++ tic-tac-toe/src/Components/Square.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Square = ({ value }) => {
+const Square = ({ value, onclick }) => {
     return (
-        <button type="button" className='square'>{value}</button>
-    )
-}
+        <button
+            type='button'
+            className='square'
+            onClick={onclick}
+        >
+            {value}
+        </button>
+    );
+};
 
 export default Square;
